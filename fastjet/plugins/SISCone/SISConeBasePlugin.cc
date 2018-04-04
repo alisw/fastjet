995616012561433cd78b373ff6ffc90d15c32a8f
hristov <Peter.Hristov@cern.ch>
2018-04-04 11:24:06 +0200

    Use only the typename, StructureType is added automatically

--- fastjet/plugins/SISCone/SISConeBasePlugin.cc
+++ fastjet/plugins/SISCone/SISConeBasePlugin.cc
@@ -9,8 +9,8 @@ FASTJET_BEGIN_NAMESPACE      // defined in fastjet/internal/base.hh
 // By default this does a simple direct comparison but it can be
 // overloaded for higher precision [recommended if possible]
 bool SISConeBasePlugin::UserScaleBase::is_larger(const PseudoJet & a, const PseudoJet & b) const{
-  return a.structure_of<UserScaleBase::StructureType>().ordering_var2()
-       > b.structure_of<UserScaleBase::StructureType>().ordering_var2();
+  return a.structure_of<UserScaleBase>().ordering_var2()
+       > b.structure_of<UserScaleBase>().ordering_var2();
 }
 
 FASTJET_END_NAMESPACE
